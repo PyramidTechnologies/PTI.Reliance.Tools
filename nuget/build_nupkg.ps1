08fa107fd5f8261436a5b06a602c413c332a674b
Cory Todd <cory@pyramidacceptors.com>
2018-05-31 19:34:44 -0700

    move all nuget build to root of project
    invoke ps1 scripts with relative notation for appveyor

--- nuget/build_nupkg.ps1
+++
@@ -1,4 +0,0 @@
-# Build a nupkg
-
-del *.nupkg
-msbuild /t:DebugVars /t:pack /p:Configuration=Release /p:PackageOutputPath=$PSScriptRoot ..\PTIRelianceLib\PTIRelianceLib.csproj
--- tools/release_nuget.ps1
+++ tools/release_nuget.ps1
@@ -1,10 +1,13 @@
-del build\*.nupkg
-dotnet pack -c Debug -o ..\build /p:Platform=x64 --include-symbols --include-source PTIRelianceLib\PTIRelianceLib.csproj
+$path = Join-Path (get-item $PSScriptRoot).parent.FullName nuget 
+if( Test-Path -Path $path )
+{
+	del $path\*.nupkg
+}
+dotnet pack -c Debug -o $path /p:Platform=x64 --include-symbols --include-source PTIRelianceLib\PTIRelianceLib.csproj
 
 # Either myself or nuget are too stupid to figure out how to easily push my package + symbols
-# List the files in PTIRelianceLib\build, there should be exactly two.
+# List the files in nuget, there should be exactly two.
 # Once is the actual library and the other is the symbols. Push just the nuget.
-
 Get-ChildItem $path -recurse | where {$_.name -notlike "*symbols*"} | % {
- #nuget push $_.FullName -Source https://api.nuget.org/v3/index.json
+	Write-Host $_.FullName #nuget push $_.FullName -Source https://api.nuget.org/v3/index.json
 }
