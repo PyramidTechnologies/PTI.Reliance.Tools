7012f5439fd53113d33ac807492b1da9d6f860da
Cory Todd <cory@pyramidacceptors.com>
2018-05-31 19:38:11 -0700

    more appveyor fixes

--- tools/release_nuget.ps1
+++ tools/release_nuget.ps1
@@ -9,5 +9,5 @@ dotnet pack -c Debug -o $path /p:Platform=x64 --include-symbols --include-source
 # List the files in nuget, there should be exactly two.
 # Once is the actual library and the other is the symbols. Push just the nuget.
 Get-ChildItem $path -recurse | where {$_.name -notlike "*symbols*"} | % {
-	Write-Host $_.FullName #nuget push $_.FullName -Source https://api.nuget.org/v3/index.json
+	nuget push $_.FullName -Source https://api.nuget.org/v3/index.json
 }
