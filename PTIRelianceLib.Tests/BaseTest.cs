d815fce3ee6acf42c491340fe84027239858a28b
Cory Todd <cory@pyramidacceptors.com>
2018-07-20 11:38:50 -0700

    Change library to netstandard 2.0
    This supports the framework shown in this matrix:
    https://docs.microsoft.com/en-us/dotnet/standard/net-standard#net-implementation-support
    
    Update project layout to support runtimes/rid pattern
    
    There are conflicting nuget docs but basically this folder layout
    with the runtimes/{rid}/native structure *should* make nuget happy.
    Initial testing shows that the dlls are correctly picked up and packed
    into the nuget. The remaining task is how to get the correct version
    copied back to the consumers output directory.
    
    Create the targets file and reference in main csproj. Key notes:
    * targets file name must match library name (e.g. PTIRelianceLib)
    * targets file must be in build/{framework} directory relative to csproj
    * runtimes is the way to go, not the lib structure
    
    Explicitly state c calling convection in NativeMethods.cs to resolve the
    "unbalanced stack" exception found on x86 builds
    
    CI was using the same build directory and the nupkg source, this conflicts
    with new build target setup. Switch CI to use dist instead.
    
    Fix CI test runner: Test projet is not properly deserializing binary assets, try
    embedding them instead. Tests still run and pass locally. Force include binary
    assets in tests project and refactor tests to manually load test resources from
    assembly instead of disk.
    
    CI build is fixed, update the version to 1.0 to match nuget

---
+++ PTIRelianceLib.Tests/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿namespace PTIRelianceLib.Tests
+{
+    using System.Reflection;
+
+    public class BaseTest
+    {
+        private readonly Assembly _thisAsm;
+        public BaseTest()
+        {
+            _thisAsm = Assembly.GetExecutingAssembly();
+        }
+
+        protected byte[] GetResource(string name)
+        {
+            var id = string.Format("PTIRelianceLib.Tests.Resources.{0}", name);
+            var stream = _thisAsm.GetManifestResourceStream(id);
+            var result = new byte[stream.Length];
+            stream.Read(result, 0, result.Length);
+            return result;
+        }
+    }
+}
