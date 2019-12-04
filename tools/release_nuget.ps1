b0a021329c53cfbd922db65cd8474f883be06200
Cory Todd <cory@pyramidacceptors.com>
2019-12-04 09:36:47 -0700

    switch to dotnet nuget deploy

--- tools/release_nuget.ps1
+++ tools/release_nuget.ps1
@@ -1,4 +1,4 @@
-$path = Join-Path (get-item $PSScriptRoot).parent.FullName nuget 
+$path = Join-Path (get-item $PSScriptRoot).parent.FullName nuget
 if( Test-Path -Path $path )
 {
 	del $path\*.nupkg
@@ -9,8 +9,8 @@ dotnet pack -c Release -o $path --include-symbols --include-source PTIRelianceLi
 # List the files in nuget, there should be exactly two.
 # Once is the actual library and the other is the symbols. Push just the nuget.
 Get-ChildItem $path -recurse | where {$_.name -notlike "*symbols*"} | % {
-	nuget push $_.FullName -Source https://api.nuget.org/v3/index.json
+	dotnet nuget push $_.FullName -s https://api.nuget.org/v3/index.json
 }
 Get-ChildItem $path -recurse | where {$_.name -like "*symbols*"} | % {
-	nuget push $_.FullName -source https://nuget.smbsrc.net/
+	dotnet nuget push $_.FullName -s https://nuget.smbsrc.net/
 }
