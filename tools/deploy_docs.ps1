676910d0c59051f0c9715fd4643cacfd78093e32
Cory Todd <cory@pyramidacceptors.com>
2020-03-06 09:06:01 -0700

    Update access token

--- tools/deploy_docs.ps1
+++ tools/deploy_docs.ps1
@@ -1,12 +1,12 @@
 git config --global credential.helper store
 Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
-config --global user.name "Bert Bot"
+git config --global user.name "Bert Bot"
 git config --global user.email "bertbot@pyramidacceptors.com"
 git clone https://github.com/PyramidTechnologies/PTI.Reliance.Tools.git -b gh-pages _site -q
 docfx docsource\docfx.json
 CD _site
 git add -A
 git diff-index --quiet HEAD
-if (0 -ne $LASTEXITCODE) { 
-	git commit -m "CI Updates" -q -and git push origin gh-pages -q
+if (0 -ne $LASTEXITCODE) {
+	git commit -m "CI Updates" -and git push origin gh-pages -q
 }
